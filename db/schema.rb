fcfe07475d4df79028434dd0cce7fee27c442c9c
AndyRama <andyramaroson@gmail.com>
2021-07-21 23:27:21 +0200

    add migration create communities

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_143510) do
+ActiveRecord::Schema.define(version: 2021_07_21_212348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.text "rules"
+    t.integer "total_members"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
