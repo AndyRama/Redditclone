0d31c3150321d2931a06d646ad6c36fc7607633f
AndyRama <andyramaroson@gmail.com>
2021-07-25 17:57:46 +0200

    home page and add profile username

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,9 @@ class PagesController < ApplicationController
     @communities = Community.all.limit(5)
     @posts = Post.order(id: :desc).limit(20)
   end
+  
+  def profile
+    @profile = User.find_by_username params[:username]
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "u/:username" => "pages#profile", as: :profile
+
   resources :communities do
     resources :posts
   end
+
   root to: 'pages#home'
 end
