5e0fbb72ecee243503dc3b1ffd163ab986139670
AndyRama <andyramaroson@gmail.com>
2021-07-25 23:57:41 +0200

    clean code home and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get "u/:username" => "pages#profile", as: :profile
+  get "u/:username" => "pages#profile", as: :profile
 
   resources :communities do
     resources :posts
