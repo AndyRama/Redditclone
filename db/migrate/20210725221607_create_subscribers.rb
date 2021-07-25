d54f98342ab033d6309d6c09da24ac3308acee60
AndyRama <andyramaroson@gmail.com>
2021-07-26 00:17:58 +0200

    add migration create_subscribers

---
+++ db/migrate/20210725221607_create_subscribers.rb
@@ -0,0 +1,10 @@
+class CreateSubscribers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :subscribers do |t|
+      t.references :user
+      t.references :community
+
+      t.timestamps
+    end
+  end
+end
