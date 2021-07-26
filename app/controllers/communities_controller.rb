14fe62598dde446b81942f48a450f0dc42dfd83a
AndyRama <andyramaroson@gmail.com>
2021-07-26 17:56:19 +0200

    add subscribers for community

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -9,6 +9,7 @@ class CommunitiesController < ApplicationController
   def show
     @posts = @community.posts
     @subscriber_count = @community.subscribers.count
+    @is_subscribed = user_signed_in? ? Subscription.where(community_id: @community.id, user_id: current_user.id).any? : false
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     resources :posts
   end
 
+  resources :subscriptions
   root to: 'pages#home'
 end
